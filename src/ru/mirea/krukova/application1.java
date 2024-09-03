5a6df4078c56c5a6a13e8e9a0ccc9461324dfe4e
pupupu <ltyutyushkal@yandex.ru>
2024-09-03 21:06:42 +0300

    мда

--- src/ru/mirea/krukova/application1.java
+++ src/ru/mirea/krukova/application1.java
@@ -1,4 +1,54 @@
-package ru.mirea.krukova;
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
 
 public class application1 {
+    public static void main(String[] args){
+        //Задание 1.
+        System.out.print("Введите размер массива: ");
+        Scanner sc = new Scanner(System.in);
+        int len = sc.nextInt();
+        int[] myArray = new int[len];
+
+        //если массив вводится с клавиатуры:
+        /*System.out.println("Введите элементы массива:");
+        for(int i = 0; i < len; i++){
+            myArray[i] = sc.nextInt();
+        }*/
+
+        //если заполняется случайно:
+        Random rd = new Random();
+        for(int i = 0; i < len; i++) {
+            myArray[i] = rd.nextInt(-10, 10);
+        }
+        Arrays.sort(myArray);
+        System.out.println("Исходный массив: " + Arrays.toString(myArray));
+        System.out.print("Введите число для поиска: ");
+        int numToFind = sc.nextInt();
+        Search(myArray, numToFind);
+    }
+    public static void Search(int[] array, int number){
+        int min = 0;
+        int max = array.length - 1; //вроде очепятка в документе с кодом
+        int average;
+
+        int count = 0; //Задание 2.
+        while(min <= max){
+            average = (min + max) / 2;
+            int num = array[average];
+            if (num == number){
+                System.out.println("Данное значение находится в массиве под индексом " + average);
+                System.out.println("Количество операций: " + count);
+                return;
+            } else if (num > number){
+                max = average - 1;
+                count++;
+            } else {
+                min = average + 1;
+                count++;
+            }
+        }
+        System.out.println("Даннов значение отсутствует в массиве.");
+    }
 }
+
--- src/ru/mirea/krukova/application2.java
+++ src/ru/mirea/krukova/application2.java
@@ -1,4 +1,64 @@
 package ru.mirea.krukova;
 
+import java.util.*;
+
 public class application2 {
+    public static void main(String[] args){
+        //Задание 3.
+        System.out.print("Введите длину массивов: ");
+        Scanner sc = new Scanner(System.in);
+        int len = sc.nextInt();
+        int[] arrayFirst = new int[len];
+        Random rd = new Random();
+        for(int i = 0; i < len; i++){
+            arrayFirst[i] = rd.nextInt(-10, 10);
+        }
+        Arrays.sort(arrayFirst);
+        System.out.println("Первый массив: " + Arrays.toString(arrayFirst));
+
+        int[] arraySecond = new int[len];
+        for(int i = 0; i < len; i++){
+            arraySecond[i] = rd.nextInt(-10, 10);
+        }
+        Arrays.sort(arraySecond);
+        System.out.println("Второй массив: " + Arrays.toString(arraySecond));
+
+        ArrayList<Integer> numbers = sameNumbers(arrayFirst, arraySecond);
+
+        System.out.print("Повторяющиеся элементы: ");
+        for (int num : numbers) { //вывод чисел в строку.
+            System.out.print(num + " ");
+        }
+        System.out.println();
+        sameNumbers(arrayFirst, arraySecond);
+
+        //при простом способе повторяющиеся числа могут выводиться большее количество раз, чем на деле (при условии, что в одном из массивов данное число повторяется n-ое кол-во раз, а в другом < n раз)
+        /* (int i = 0; i < len; i++){
+            for (int j = 0; j < len; j++){
+                if (arrayFirst[i] == arraySecond[j]){
+                    System.out.print(arrayFirst[i] + " ");
+                }
+            }
+        }*/
+
+    }
+
+    public static ArrayList<Integer> sameNumbers(int[] arr1, int[] arr2) {
+        //Задание 4.
+        Set<Integer> set1 = new HashSet<>();
+        Set<Integer> set2 = new HashSet<>();
+
+        for (int num : arr1) {
+            set1.add(num);
+        }
+
+        for (int num : arr2) {
+            set2.add(num);
+        }
+
+        Set<Integer> sameNums = new HashSet<>(set1);
+        sameNums.retainAll(set2);
+
+        return new ArrayList<>(sameNums);
+    }
 }
